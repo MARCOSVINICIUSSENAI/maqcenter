e6797ab6d1a0b72faca82399ec8228149ebd661d
MARCOS VINICIUS <patriciniodasiLva@GMAIL.COM>
2024-07-22 17:33:14 -0300

    Back visual studio

---
+++ Elora/Backup visual studio/MaqCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaqCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
